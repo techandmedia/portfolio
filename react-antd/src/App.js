6b10a09bf2c7f3776de94e58d97f594dd34502df
Eko Andri Subarnanto <eko.andri@icloud.com>
2018-12-29 20:34:06 +0700

    Scaffolding React App with react-app-rewired

--- react-antd/src/App.js
+++ react-antd/src/App.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Spin, Alert } from "antd";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <Spin tip="Loading...">
+      <h1 style={{ textAlign: "center", marginBottom: 50, marginTop: 20 }}>
+        A scaffolding React with react-app-rewired
+      </h1>
+      <Alert
+        message="Alert message title"
+        description="Further details about the context of this alert."
+        type="info"
+      />
+    </Spin>
+  );
 }
 
 export default App;
